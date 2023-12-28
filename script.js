0f1f22441e04bbe10752ab9ed097f62a02e2e47a
kavya rajput <131941640+kavyarajput30@users.noreply.github.com>
2023-12-28 18:28:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
-
-
-
-
 let btn = document.querySelector("button");
-btn.addEventListener("click",()=>{
-
+btn.addEventListener("click", () => {
     let wght = document.querySelector("#weight");
     let hight = document.querySelector("#height");
     let bmi = document.querySelector(".bmi");
-    let calculate = (wght.value)/((hight.value)*(hight.value));
-    bmi.innerHTML= calculate;
-    wght.value="";
-    hight.value="";
+    
+    let calculate = (wght.value) / ((hight.value) * (hight.value));
+    bmi.innerHTML = calculate;
+    wght.value = "";
+    hight.value = "";
      
-    let cond= document.querySelector(".condition")
-    if(calculate<18.6){ 
-        cond.innerHTML= "Under Weight";
+    let cond = document.querySelector(".condition");
+    if (calculate < 18.6) { 
+        cond.innerHTML = "Under Weight";
+    } else if (calculate < 24.9 && calculate > 18.6) {
+        cond.innerHTML = "Normal Weight";
+    } else if (calculate > 24.9) {
+        cond.innerHTML = "OverWeight";
     }
-    else if( calcualte<24.9 && calculate>18.6){
-        cond.innerHTML= "Normal Weight";
-    }
-    else if(calculate>24.9){
-        cond.innerHTML= "OverWeight";
-    }
-})
+});
 
 
 
